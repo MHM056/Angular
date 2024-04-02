9f5344bbdfb27416916b0d4195af4af9bee6a413
MHM056 <mihailmarholev@gmail.com>
2024-04-03 00:47:44 +0300

    4.3 Implement the Read More Function

--- Components Lab/article-app/src/app/article/article.component.ts
+++ Components Lab/article-app/src/app/article/article.component.ts
@@ -8,8 +8,8 @@ import { Article } from '../models/article.model';
 })
 export class ArticleComponent {
   private symbols: number = 250;
-  @Input() article: Article;
-  @Input() articleDesc: string;
+  @Input() article!: Article;
+  @Input() articleDesc!: string;
   descToShow: string;
   articleDescLen: number;
   showReadMoreBtn: boolean = true;
@@ -22,4 +22,14 @@ export class ArticleComponent {
     this.descToShow = "";
   }
    
+  readMore(): void {
+    this.articleDescLen += this.symbols;
+
+    if(this.articleDescLen >= this.articleDesc.length) {
+      this.showHideBtn = true;
+      this.showReadMoreBtn = false;
+    } else {
+      this.descToShow = this.articleDesc.substring(0, this.articleDescLen);
+    }
+  }
 }
