0266263a0c43a5d5955eade12543c14f60bdb0a9
MHM056 <mihailmarholev@gmail.com>
2024-04-03 00:39:26 +0300

    4.2 Create the Needed Properties

--- Components Lab/article-app/src/app/article/article.component.ts
+++ Components Lab/article-app/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from '../models/article.model';
 
 @Component({
   selector: 'app-article',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent {
+  private symbols: number = 250;
+  @Input() article: Article;
+  @Input() articleDesc: string;
+  descToShow: string;
+  articleDescLen: number;
+  showReadMoreBtn: boolean = true;
+  showHideBtn: boolean = false;
+  imageIsShown: boolean = false;
+  imageButtonTitle: string = "Show Image";
 
+  constructor() {
+    this.articleDescLen = 0;
+    this.descToShow = "";
+  }
+   
 }
