4629ec48acf7e5616e0fa9f53e52749e9b2f927a
MHM056 <mihailmarholev@gmail.com>
2024-04-02 22:51:40 +0300

    4. Create Article Component

--- Components Lab/article-app/src/app/app.module.ts
+++ Components Lab/article-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule
