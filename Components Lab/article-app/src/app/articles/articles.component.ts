bb60c9bfcfaa988abc6b209e4129923e08bf3ffc
MHM056 <mihailmarholev@gmail.com>
2024-04-03 21:48:21 +0300

    5.1 Implement Logic Inside the Component

--- Components Lab/article-app/src/app/articles/articles.component.ts
+++ Components Lab/article-app/src/app/articles/articles.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article } from '../models/article.model';
+import { ArticleData } from '../data/data';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.css']
 })
-export class ArticlesComponent {
+export class ArticlesComponent implements OnInit {
+    articles!: Article[];
 
+    constructor() {}
+
+    ngOnInit(): void {
+      this.articles = new ArticleData().getData();  
+    }
 }
