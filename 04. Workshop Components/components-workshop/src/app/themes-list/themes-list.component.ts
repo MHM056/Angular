b6f75958ba11b49ee7ca9a187c157d8b27ab9a01
MHM056 <mihailmarholev@gmail.com>
2024-04-07 21:12:46 +0300

    Update themes-list.component.ts

--- 04. Workshop Components/components-workshop/src/app/themes-list/themes-list.component.ts
+++ 04. Workshop Components/components-workshop/src/app/themes-list/themes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-themes-list',
@@ -6,5 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./themes-list.component.css']
 })
 export class ThemesListComponent implements OnInit {
+  constructor(private api: ApiService ) {}
 
+  ngOnInit(): void {
+    this.api.getThemes().subscribe(themes => console.log(themes))
+  }
 }
