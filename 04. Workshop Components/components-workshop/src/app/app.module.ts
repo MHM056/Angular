8d6e9ca50a81effcee8e6b1edbf35f28f7c8751a
MHM056 <mihailmarholev@gmail.com>
2024-04-07 20:26:22 +0300

    Created main component

--- 04. Workshop Components/components-workshop/src/app/app.module.ts
+++ 04. Workshop Components/components-workshop/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
