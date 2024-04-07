b2bfb35cea20c8afec64191b01fe6ba045044bf5
MHM056 <mihailmarholev@gmail.com>
2024-04-07 20:27:31 +0300

    Created themes-list and posts-list components

--- 04. Workshop Components/components-workshop/src/app/app.module.ts
+++ 04. Workshop Components/components-workshop/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { MainComponent } from './main/main.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    ThemesListComponent,
+    PostsListComponent
   ],
   imports: [
     BrowserModule,
