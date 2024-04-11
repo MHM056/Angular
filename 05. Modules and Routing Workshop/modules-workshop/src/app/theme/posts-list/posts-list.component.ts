1681790c660badb7197c308d93f8fb9f4c1ce81f
MHM056 <mihailmarholev@gmail.com>
2024-04-11 22:36:25 +0300

    Moved posts-list and main components to the themes module

--- 05. Modules and Routing Workshop/modules-workshop/src/app/theme/posts-list/posts-list.component.ts
+++ 05. Modules and Routing Workshop/modules-workshop/src/app/theme/posts-list/posts-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
-import { Post } from '../types/post';
+import { ApiService } from '../../api.service';
+import { Post } from '../../types/post';
 
 @Component({
   selector: 'app-posts-list',
