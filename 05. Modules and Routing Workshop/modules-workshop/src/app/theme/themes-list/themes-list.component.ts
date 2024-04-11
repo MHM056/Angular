40ad0b4628fa82befa7b988f2365330801290a24
MHM056 <mihailmarholev@gmail.com>
2024-04-11 22:37:13 +0300

    Moved themes-list component to the theme module

--- 05. Modules and Routing Workshop/modules-workshop/src/app/theme/themes-list/themes-list.component.ts
+++ 05. Modules and Routing Workshop/modules-workshop/src/app/theme/themes-list/themes-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
-import { Theme } from '../types/theme';
+import { ApiService } from '../../api.service';
+import { Theme } from '../../types/theme';
 
 @Component({
   selector: 'app-themes-list',
