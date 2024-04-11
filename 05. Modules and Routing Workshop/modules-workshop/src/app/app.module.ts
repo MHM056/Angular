37245587f0b3b0d198d3ebde140e465c272cd4d1
MHM056 <mihailmarholev@gmail.com>
2024-04-11 23:29:58 +0300

    Created error component

--- 05. Modules and Routing Workshop/modules-workshop/src/app/app.module.ts
+++ 05. Modules and Routing Workshop/modules-workshop/src/app/app.module.ts
@@ -9,11 +9,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { UserModule } from './user/user.module';
 import { ThemeModule } from './theme/theme.module';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
