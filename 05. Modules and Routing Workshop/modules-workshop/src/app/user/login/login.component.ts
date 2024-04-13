8f4cac7042c98c40d938ab497c6a0c2fb45b343e
MHM056 <mihailmarholev@gmail.com>
2024-04-13 21:59:34 +0300

    Added logout and fake login functionality

--- 05. Modules and Routing Workshop/modules-workshop/src/app/user/login/login.component.ts
+++ 05. Modules and Routing Workshop/modules-workshop/src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private userService: UserService) {}
 
+  login(ev: Event,  email: string, password: string) {
+    ev.preventDefault();
+    this.userService.login();
+  }
 }
+
