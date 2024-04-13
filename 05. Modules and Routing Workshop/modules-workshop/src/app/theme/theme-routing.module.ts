f508df30b8f67ff75e0d998d1acd818d5db92101
MHM056 <mihailmarholev@gmail.com>
2024-04-13 21:39:10 +0300

    Added html and css for current-theme component

--- 05. Modules and Routing Workshop/modules-workshop/src/app/theme/theme-routing.module.ts
+++ 05. Modules and Routing Workshop/modules-workshop/src/app/theme/theme-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { MainComponent } from "./main/main.component";
 import { AddThemeComponent } from "./add-theme/add-theme.component";
+import { CurrentThemeComponent } from "./current-theme/current-theme.component";
 
-const routes: Routes = [{ path: 'themes', component: MainComponent},
-    { path: 'add-theme', component: AddThemeComponent}
+const routes: Routes = [{ path: 'themes', component: MainComponent,},
+    { path: 'add-theme', component: AddThemeComponent},
+    { path: 'themes/:themeId', component: CurrentThemeComponent}
 ];
 
 @NgModule({
