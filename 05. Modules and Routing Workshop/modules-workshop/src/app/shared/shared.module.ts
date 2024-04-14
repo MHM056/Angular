bb3693183ed4dcae0f83d6d7b209551355228df0
MHM056 <mihailmarholev@gmail.com>
2024-04-14 20:59:53 +0300

    Created reusable welcome component

--- 05. Modules and Routing Workshop/modules-workshop/src/app/shared/shared.module.ts
+++ 05. Modules and Routing Workshop/modules-workshop/src/app/shared/shared.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 
 @NgModule({
   declarations: [
-    LoaderComponent
+    LoaderComponent,
+    WelcomeComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [LoaderComponent]
+  exports: [LoaderComponent, WelcomeComponent]
 })
 export class SharedModule { }
---
+++ 05. Modules and Routing Workshop/modules-workshop/src/app/shared/welcome/welcome.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent {
+  @Input('isLoggedIn') isLoggedIn = false;
+}
