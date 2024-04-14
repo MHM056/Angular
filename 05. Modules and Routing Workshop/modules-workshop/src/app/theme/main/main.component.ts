e28a35bcf3270a158243b92c9aa669b29ce79aa2
MHM056 <mihailmarholev@gmail.com>
2024-04-14 21:19:12 +0300

    Updated main component
    - Only logged in users can review the main page

--- 05. Modules and Routing Workshop/modules-workshop/src/app/theme/main/main.component.ts
+++ 05. Modules and Routing Workshop/modules-workshop/src/app/theme/main/main.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
-
+export class MainComponent implements OnInit{
+  constructor(private userService: UserService, private route: Router) {}
+  ngOnInit(): void {
+    const isLogged: boolean = this.userService.isLogged;
+    
+    if(!isLogged) {
+      alert('You need to be logged in to review this page');
+      this.route.navigate(['/home']);
+    }
+  }
 }
