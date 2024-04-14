2389cfa0cc2bae6871132dbf0a5613205c208d8c
MHM056 <mihailmarholev@gmail.com>
2024-04-14 20:48:55 +0300

    Login and Register buttons are removed from Home page when logged in

--- 05. Modules and Routing Workshop/modules-workshop/src/app/home/home.component.ts
+++ 05. Modules and Routing Workshop/modules-workshop/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(private userService: UserService) {}
+
+  get isLoggedIn(): boolean {
+
+    return this.userService.isLogged;
+  }
 }
