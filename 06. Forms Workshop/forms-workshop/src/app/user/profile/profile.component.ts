446d3d6d7537e01c5e247fea3e637a6a4075a59e
MHM056 <mihailmarholev@gmail.com>
2024-04-21 22:24:07 +0300

    Added profile component functionalities
    - Actual user's data is now being rendered on init;
    - Edit form is shown and hidden based on edit/cancel button;

--- 06. Forms Workshop/forms-workshop/src/app/user/profile/profile.component.ts
+++ 06. Forms Workshop/forms-workshop/src/app/user/profile/profile.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
+  showEditMode: boolean = false;
+
+  username: string = '';
+  email: string = '';
+  phoneNumber: string = '';
+
+  ngOnInit(): void {
+    const getUserDetails: any = localStorage.getItem('[user]');
+    const userDetails = JSON.parse(getUserDetails);
+    this.username = userDetails.firstName;
+    this.email = userDetails.email;
+    this.phoneNumber = userDetails.phoneNumber;
+  }
+
+  onEdit() {
+    this.showEditMode = true;
+  }
+
+  onCancel() {
+    this.showEditMode = false;
+  }
 }
